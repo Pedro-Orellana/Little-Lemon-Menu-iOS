7c91d0345ec91c41c6567f40b22a35fc3b49be51
Pedro Orellana <pedrorellana21@gmail.com>
2024-01-30 11:59:07 -0800

    Initial Commit

---
+++ Little Lemon Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Little Lemon Menu
+//
+//  Created by Pedro Orellana on 1/30/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Little Lemon Menu/Little_Lemon_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Little_Lemon_MenuApp.swift
+//  Little Lemon Menu
+//
+//  Created by Pedro Orellana on 1/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Little_Lemon_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
